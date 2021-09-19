42aa225c200eb00dbe2512232b907dcdefa569a9
Ignacio Diaz <nachodiaz99@gmail.com>
2021-09-19 16:25:10 +1000

    Functions saveInfo and loadInfo working fine. Setting color to rows working, It needs to be checked at any other time to make sure it works 100%

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,55 +1,81 @@
 var $currentDay = $("#currentDay");
-$currentDay.text(moment().format("D MMM YYYY")); //Display current date on screen
+$currentDay.text(moment().format("dddd, Do MMM YYYY, kk:mm:ss")); //Display current date on screen
 
-const dailyTimes = ["8 AM", "9 AM", "10 AM", "11AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+const dailyTimes = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+var descriptionArray = ['','','','','','','','','',''];
 var $containerElement = $(".container");
-var localTime = moment().format("LT"); // Current local time
+var localTime = moment()// Current local time
 
 // Starts the page, loads all rows and their stored information
 function displayTimes() {
     var $row, $timeBlock, $textArea, $saveBtn;
     dailyTimes.forEach(function(element, index){
-        $row = $("<div>").addClass("row");
+        $row = $("<div>").addClass("row hour");
         $containerElement.append($row);
 
-        $timeBlock = $("<div>").addClass("time-block").text(element);
+        $timeBlock = $("<div>").addClass("time-block col-1").text(element);
         $row.append($timeBlock);
 
-        $textArea = $("<textarea>").addClass("description");
+        $textArea = $("<textarea>").addClass("description col-10");
+        $textArea.attr('data-index',index);
         $row.append($textArea);
 
-        $saveBtn = $("<button>").addClass("saveBtn");
+        $saveBtn = $("<button>").addClass("saveBtn col-1");
         $row.append($saveBtn);
 
-        //checkTime();
+        // Set color for each row according to time
+        if(element < localTime.format('kk')){
+            $textArea.addClass('past');
+        }else if(element == localTime.format('kk')){
+            $textArea.addClass('present');
+        }else{
+            $textArea.addClass('future');
+        }
     })
 }
 
-// function to save information written on local Storage
+// function to save information written on local Storage. I use JSON to save the whole array where I save information users input
 function saveInfo() {
     $(".saveBtn").click(function(){
         var btnClicked = $(this);
-        var text = btnClicked.siblings(".description").val();
-        localStorage.setItem("task", text);
+        var textWritten = btnClicked.siblings(".description").val();
+        var textIndex = btnClicked.siblings('.description').data('index');
+
+        // I need to overwrite everything saved on local storage, otherwise info will be lost
+        var localArray = JSON.parse(localStorage.getItem('tasks'));
+
+        // Check if this is the first time using the APP
+        if(localArray == null){
+            descriptionArray[textIndex] = textWritten;
+            localStorage.setItem('tasks',JSON.stringify(descriptionArray));
+            return;
+        }
+
+        for(var i=0; i<descriptionArray.length; i++){
+            descriptionArray[i] = localArray[i];
+        }
+
+        descriptionArray[textIndex] = textWritten;
+        localStorage.setItem('tasks',JSON.stringify(descriptionArray));
     });
 }
 
+// function to load all information saved on local storage and display on screen
 function loadInfo() {
+    // This is not JQuery
     var textArray = document.querySelectorAll(".description");
-    console.log(textArray);
+    var localArray = JSON.parse(localStorage.getItem('tasks'));
+
+    // Check if this is the first time using the APP
+    if(localArray == null){
+        return;
+    }
 
     textArray.forEach(function(element, index){
-        var task = localStorage.getItem("task");
-        console.log(task);
+        element.textContent = localArray[index];
     });
 }
-
-// function to color rows according to current time
-function checkTime() {
-
-}
-
-// if("11 AM" < localTime && localTime < "12 PM")way to know range of time it is at a certain time
 
 displayTimes();
 loadInfo();
